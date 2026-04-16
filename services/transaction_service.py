0847d0eea6d8c587240599cbdf3653dd1f2b689a
aristos725 <62718948+aristos725@users.noreply.github.com>
2026-04-16 18:24:21 +0300

    Update transaction_service.py

---
+++ services/transaction_service.py
@@ -0,0 +1,9 @@
+def add_transaction(amount, transaction_type, category, date, description):
+    print(
+        "Saving transaction:",
+        amount,
+        transaction_type,
+        category,
+        date,
+        description
+    )
